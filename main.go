9eb904cd78012b4336bad01e1f3d3badc8cfc4a5
Karthik1503 <56954632+Karthik1503@users.noreply.github.com>
2019-11-10 13:58:49 +0530

    Add division method
    1. Add division with `/` operator.
    2. Add division with subtraction.

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func subtract(x, y int) int {
 	return x - y
 }
 
+
 func multiply(x, y int) int {
 	index := 0
 	result := 0
@@ -18,8 +19,28 @@ func multiply(x, y int) int {
 	return result
 }
 
+func divide(x, y int) int {
+
+	result := 0
+	if x < y {
+		result = 0
+	} else if x == y {
+		result = 1
+	} else {
+		for x > 0 && x > y {
+			result += 1
+			subtract(x, y)
+		}
+	}
+	return result
+}
+
+
 func simpleMultiply(x, y int) int {
 	return x * y
 }
 
+func simpleDivide(x, y int) int {
+	return x % y
+}
 
